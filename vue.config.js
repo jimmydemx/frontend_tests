732ad0a260af2fcb8f1257cf0ac482be1236f08c
jimmydemx <j_demx@yahoo.com>
2021-08-03 17:42:18 +0200

    1.2 config router

--- vue.config.js
+++ vue.config.js
@@ -1,29 +1,23 @@
-const path= require('path')
+const path = require("path");
 
+module.exports = {
+    configureWebpack: config => {
+        if (process.env.NODE_ENV === "production") {
 
-module.exports={
-    configureWebpack: config=>{
-        if(process.env.NODE_ENV === "production"){
-
-        }else{
-
+        } else {
             return {
-                
-                resolve:{
-                    alias:{
-                        '@js':path.resolve(__dirname,"./src/assets/js"),
-                        '@css':path.resolve(__dirname,"./src/assets/css"),
-                        '@img':path.resolve(__dirname,"./src/assets/img"),
-                        "@c":path.resolve(__dirname,"./src/components"),
+
+                resolve: {
+                    alias: {
+                        "@js": path.resolve(__dirname, "./src/assets/js"),
+                        "@css": path.resolve(__dirname, "./src/assets/css"),
+                        "@img": path.resolve(__dirname, "./src/assets/img"),
+                        "@c": path.resolve(__dirname, "./src/components")
 
                     }
                 }
-            }
+            };
         }
-    }, 
+    }
 
-}
-
-
-
-
+};
