e294360d20d325658458a9a1e4071c95c68b1288
jimmydemx <j_demx@yahoo.com>
2021-08-03 19:06:23 +0200

    1.5 change  folder

---
+++ src/assets/js/axios.config.js
@@ -0,0 +1,19 @@
+import axios from 'axios'
+import Vue from "vue"
+
+
+// set default URL address
+axios.defaults.baseURL="http://51.38.51.187:5050/api/v1/"
+
+//  interceptors all request will be first put in interceptors
+axios.interceptors.response.use((res) => {
+    return res.data;
+}, (error) => {
+    return Promise.reject(error);  
+});
+
+Vue.prototype.$axios=axios
+
+
+
+
